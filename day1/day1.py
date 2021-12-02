837e496e83aec51f0e81fda086fa64acfe6ace67
Kirklin <kirklin@reportmate.ca>
2021-12-02 20:21:05 -0330

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,36 @@
+
+import sys
+input = open("input.txt", "r")
+
+sys.setrecursionlimit(5000)
+    
+# PART A
+def count(line):
+    value = input.readline()
+    if value == "":
+        return 0
+    elif int(value)> int(line):
+        return 1 + count(value)
+    else:
+        return 0 + count(value)
+    
+#print(count(input.readline()))
+
+#PART B
+
+def slidingSums(a,b,c):
+    nextValue = input.readline()
+    if(nextValue) == "":
+        return 0
+    prevWindow = int(a) + int(b) + int(c)
+    nextWindow = int(b) + int(c) + int(nextValue)
+
+    if nextWindow > prevWindow:
+        return 1 + slidingSums(b,c,nextValue)
+    else:
+        return 0 + slidingSums(b,c,nextValue)
+
+
+#print(slidingSums(input.readline(),input.readline(),input.readline()))
+
+input.close()
